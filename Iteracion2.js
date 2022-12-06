02e0b360f7041cf4d63413b300de0728358cd7fd
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-06 20:29:57 +0100

    Iteracion#2

--- Iteracion2.js
+++ Iteracion2.js
@@ -1,10 +1,29 @@
 "use strict";
 //Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad isApproved a true o false en consecuencia. 
 //Una vez lo tengas compruébalo con un console.log.
+
 const alumns = [
-    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
-		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+    {name: 'Pepe Viruela',		T1: false, T2: false, T3: true}, 
+	{name: 'Lucia Aranda', 		T1: true, T2: false, T3: true},
+	{name: 'Juan Miranda', 		T1: false, T2: true, T3: true},
+	{name: 'Alfredo Blanco', 	T1: false, T2: false, T3: false},
+	{name: 'Raquel Benito', 	T1: true, T2: true, T3: true}
 ]
+
+function comprobar(alumno) {
+	return (alumno.T1 === true && alumno.T2 === true || alumno.T1=== true && alumno.T3===true || alumno.T2===true && alumno.T3===true ); 
+}
+
+for (let i=0; i < alumns.length; i++ ) {
+
+    let alumno = alumns[i];
+
+    if (comprobar(alumno) ) {
+        alumno.isApproved = true; 
+    } else {
+        alumno.isApproved = false;
+    }
+
+	console.log(alumno.name + ' : isApproved: ' + alumno.isApproved);
+}
+
